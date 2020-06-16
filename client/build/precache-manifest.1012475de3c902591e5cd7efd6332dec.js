86239705b53632bb228f49887e65c66aee730ba8
Indus-Chugalaev <cii-trade@yandex.ru>
2020-06-16 10:44:49 +0500

    build

---
+++ client/build/precache-manifest.1012475de3c902591e5cd7efd6332dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971b56e8075c96c8f8603cf25ac0d9e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1cc670a9a7cc5429fcf",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "040f6084fd18b9fb7c27",
+    "url": "/static/css/main.789a585a.chunk.css"
+  },
+  {
+    "revision": "c1cc670a9a7cc5429fcf",
+    "url": "/static/js/2.627c30aa.chunk.js"
+  },
+  {
+    "revision": "14c9108fd5a38194785d545908fb6cbd",
+    "url": "/static/js/2.627c30aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "040f6084fd18b9fb7c27",
+    "url": "/static/js/main.73f29a14.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
